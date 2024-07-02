6f6d69149a14af01b0627f9de2110aa3eb7a4629
Nathnael Chanyalew <nathnaelc5@gmail.com>
2024-07-02 16:17:10 -0400

    Add the logic to let the calculator operate on numbers

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let firstNumber;
 let secondNumber;
 let operator;
-let displayValue = 0;
+let isResettable = true;
+const displayNode = document.querySelector("#display");
 
 function add(number1, number2){
     return number1 + number2;
@@ -35,16 +36,39 @@ function populateDisplay(){
     const listOfNumberButtons = document.querySelectorAll(".number");
 
     for(let i = 0; i < listOfNumberButtons.length; i++){
-    const currentNumberButton = listOfNumberButtons[i];
-    const displayNode = document.querySelector("#display");
-    currentNumberButton.addEventListener("click", () => {
-        displayNode.textContent = currentNumberButton.textContent;
-        displayValue = displayNode.textContent;
-    });
+        const currentNumberButton = listOfNumberButtons[i];
+        currentNumberButton.addEventListener("click", () => {
+            if(isResettable){
+                displayNode.textContent = currentNumberButton.textContent;
+                isResettable = false;
+            }else {
+                displayNode.textContent += currentNumberButton.textContent;
+            }
+        });
+    }
 }
+
+function updateFirstNumber(){
+    const listOfOperatorButtons = document.querySelectorAll(".operator");
+    for(let i = 0; i < listOfOperatorButtons.length; i++){
+        const operatorButton = listOfOperatorButtons[i];
+        operatorButton.addEventListener("click", () => {
+            firstNumber = Number(displayNode.textContent);
+            operator = operatorButton.textContent;
+            isResettable = true;
+        });
+    }
+}
+
+function completeCalculation(){
+    const equalsButton = document.querySelector("#equals-button");
+    equalsButton.addEventListener("click", () => {
+        secondNumber = Number(displayNode.textContent);
+        displayNode.textContent = operate(operator, firstNumber, secondNumber);
+        isResettable = true;
+    });
 }
 
 populateDisplay();
-
-
-console.log(operate("/", 6, 2));
+updateFirstNumber();
+completeCalculation();
